0852a6da5c485aaa32478dfc25e3db4d10db64a3
金山智昭 <kanayamatomoaki@kanayamatomoakinoiMac-2.local>
2019-10-01 22:16:24 +0900

    set absolute url

---
+++ build/precache-manifest.833e11dd2195627f30948c2650601e79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84a46eb4ccece681ad64e5891b445e01",
+    "url": "/harmovis_examples/build/index.html"
+  },
+  {
+    "revision": "50ff2fe79fdd8263b98a",
+    "url": "/harmovis_examples/build/static/css/2.298d93f3.chunk.css"
+  },
+  {
+    "revision": "8ca7545f963eb9ce192d",
+    "url": "/harmovis_examples/build/static/css/main.f6c9cef1.chunk.css"
+  },
+  {
+    "revision": "50ff2fe79fdd8263b98a",
+    "url": "/harmovis_examples/build/static/js/2.e98502ae.chunk.js"
+  },
+  {
+    "revision": "8ca7545f963eb9ce192d",
+    "url": "/harmovis_examples/build/static/js/main.44cd9aba.chunk.js"
+  },
+  {
+    "revision": "272c84fc2d4d7f2feccc",
+    "url": "/harmovis_examples/build/static/js/runtime-main.b5abd3c1.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.090d4503b87e4487bcc31ba798e6e3b3.js"
+  "/harmovis_examples/build/precache-manifest.833e11dd2195627f30948c2650601e79.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/harmovis_examples/build/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
